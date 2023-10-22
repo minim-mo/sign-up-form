e86a3a789febdef9ec65afb2e9b2824b45803e6a
minim-mo <147269904+minim-mo@users.noreply.github.com>
2023-10-22 11:50:31 -0400

    updated some layout form and start create function to check if the password confirm is match.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hello!")
+//create the login check if password input are the same.
+document.addEventListener("DOMContentLoaded", function(){
+    let password = document.querySelector("#password").value;
+    let confirmPassword = document.querySelector("#confirmPassword"). value;
+
+    let error = document.querySelector("#error")
+
+    if(password != confirmPassword){
+        console.log("Password did not match!")
+    }
+
+    console.log("hello!");
+})
